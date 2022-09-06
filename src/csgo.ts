c83acab94d8a2ac64dd6c5d72244976497d4187f
Ian Lucas <ianlucas_ferreira@hotmail.com>
2022-09-05 23:53:48 -0300

    feat: add GameWeaponClasses and GameKnifeClasses

--- src/csgo.ts
+++ src/csgo.ts
@@ -1,15 +1,74 @@
 export interface GameMap {
   imageSrc: string;
   label: string;
-  uuid: string;
+  id: string;
 }
 
 export const GameMapList: GameMap[] = [
-  { label: 'Mirage', imageSrc: '/csgo/mirage.jpg', uuid: 'de_mirage' },
-  { label: 'Dust II', imageSrc: '/csgo/dust2.jpg', uuid: 'de_dust2' },
-  { label: 'Inferno', imageSrc: '/csgo/inferno.jpg', uuid: 'de_inferno' },
-  { label: 'Vertigo', imageSrc: '/csgo/vertigo.jpg', uuid: 'de_vertigo' },
-  { label: 'Overpass', imageSrc: '/csgo/overpass.jpg', uuid: 'de_overpass' },
-  { label: 'Nuke', imageSrc: '/csgo/nuke.jpg', uuid: 'de_nuke' },
-  { label: 'Ancient', imageSrc: '/csgo/ancient.jpg', uuid: 'de_ancient' }
+  { label: 'Mirage', imageSrc: '/csgo/mirage.jpg', id: 'de_mirage' },
+  { label: 'Dust II', imageSrc: '/csgo/dust2.jpg', id: 'de_dust2' },
+  { label: 'Inferno', imageSrc: '/csgo/inferno.jpg', id: 'de_inferno' },
+  { label: 'Vertigo', imageSrc: '/csgo/vertigo.jpg', id: 'de_vertigo' },
+  { label: 'Overpass', imageSrc: '/csgo/overpass.jpg', id: 'de_overpass' },
+  { label: 'Nuke', imageSrc: '/csgo/nuke.jpg', id: 'de_nuke' },
+  { label: 'Ancient', imageSrc: '/csgo/ancient.jpg', id: 'de_ancient' }
+];
+
+export const GameWeaponClasses: string[] = [
+  'weapon_awp',
+  'weapon_ak47',
+  'weapon_m4a1',
+  'weapon_m4a1_silencer',
+  'weapon_deagle',
+  'weapon_usp_silencer',
+  'weapon_hkp2000',
+  'weapon_glock',
+  'weapon_elite',
+  'weapon_p250',
+  'weapon_cz75a',
+  'weapon_fiveseven',
+  'weapon_tec9',
+  'weapon_revolver',
+  'weapon_nova',
+  'weapon_xm1014',
+  'weapon_mag7',
+  'weapon_sawedoff',
+  'weapon_m249',
+  'weapon_negev',
+  'weapon_mp9',
+  'weapon_mac10',
+  'weapon_mp7',
+  'weapon_ump45',
+  'weapon_p90',
+  'weapon_bizon',
+  'weapon_famas',
+  'weapon_galilar',
+  'weapon_ssg08',
+  'weapon_aug',
+  'weapon_sg556',
+  'weapon_scar20',
+  'weapon_g3sg1',
+  'weapon_mp5sd'
+];
+
+export const GameKnifeClasses: string[] = [
+  'weapon_knife_karambit',
+  'weapon_knife_m9_bayonet',
+  'weapon_bayonet',
+  'weapon_knife_survival_bowie',
+  'weapon_knife_butterfly',
+  'weapon_knife_flip',
+  'weapon_knife_push',
+  'weapon_knife_tactical',
+  'weapon_knife_falchion',
+  'weapon_knife_gut',
+  'weapon_knife_ursus',
+  'weapon_knife_gypsy_jackknife',
+  'weapon_knife_stiletto',
+  'weapon_knife_widowmaker',
+  'weapon_knife_css',
+  'weapon_knife_cord',
+  'weapon_knife_canis',
+  'weapon_knife_outdoor',
+  'weapon_knife_skeleton'
 ];
